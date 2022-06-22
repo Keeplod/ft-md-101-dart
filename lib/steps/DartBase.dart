bd1cd1620ec7b9a1dbc9b74772615894dbc67a9d
Keeplod <Keeplod@yandex.ru>
2022-06-22 22:34:17 +0300

    Урок №9 - Наследование

--- lib/steps/DartBase.dart
+++ lib/steps/DartBase.dart
@@ -1,4 +1,5 @@
 import 'package:ft_md_101_dart/ClassInstance.dart';
+import 'package:ft_md_101_dart/steps/utils/DartChild.dart';
 import 'package:ft_md_101_dart/steps/utils/DartMethod.dart';
 import 'package:ft_md_101_dart/steps/utils/DartPackage.dart';
 import 'package:ft_md_101_dart/steps/utils/DartSignature.dart';
@@ -13,6 +14,7 @@ class DartBase{
     _stepPackage();
     _stepMethods();
     _stepSignature();
+    _stepInheritance();
   }
   void _stepConf(){
     print("_stepConf: This is step 1");
@@ -57,5 +59,9 @@ class DartBase{
     ds.first = "first after set";
     ds.second = "second after set";
     ds.run();
+  }
+  void _stepInheritance(){
+    var dc = DartChild();
+    dc.run();
   }
 }
