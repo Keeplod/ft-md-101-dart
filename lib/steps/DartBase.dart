0716c65b60a557e56c51ede3bf88b3c4b541c738
Keeplod <Keeplod@yandex.ru>
2022-06-24 14:13:37 +0300

    Задание №16

--- lib/steps/DartBase.dart
+++ lib/steps/DartBase.dart
@@ -8,6 +8,7 @@ import 'package:ft_md_101_dart/steps/tasks/Task12.dart';
 import 'package:ft_md_101_dart/steps/tasks/Task13.dart';
 import 'package:ft_md_101_dart/steps/tasks/Task14.dart';
 import 'package:ft_md_101_dart/steps/tasks/Task15.dart';
+import 'package:ft_md_101_dart/steps/tasks/Task16.dart';
 import 'package:ft_md_101_dart/steps/tasks/Task2.dart';
 import 'package:ft_md_101_dart/steps/tasks/Task3.dart';
 import 'package:ft_md_101_dart/steps/tasks/Task4.dart';
@@ -64,7 +65,8 @@ class DartBase{
     //Task12().run();
     //Task13().run();
     //Task14().run();
-    Task15().run();
+    //Task15().run();
+    Task16().run();
   }
   void _stepConf(){
     print("_stepConf: This is step 1");
